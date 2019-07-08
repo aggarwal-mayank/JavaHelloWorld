13e6f25f757aad8406fa74269122ca340413f4ac
Mayank Aggarwal <maggarwal7@sapient.com>
2019-07-08 12:32:02 +0100

    Initial Commit

---
+++ src/main/java/com/ma/programming/JavaHelloWorld/HelloController.java
@@ -0,0 +1,15 @@
+package com.ma.programming.JavaHelloWorld;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String greeting() {
+        return "Hello World";
+    }
+}
