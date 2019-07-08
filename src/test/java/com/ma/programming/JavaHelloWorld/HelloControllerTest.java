b9a64521281d61836d2b0d169ad1fa6edfb7ff41
Mayank Aggarwal <maggarwal7@sapient.com>
2019-07-08 13:58:05 +0100

    Added Test Cases

---
+++ src/test/java/com/ma/programming/JavaHelloWorld/HelloControllerTest.java
@@ -0,0 +1,26 @@
+package com.ma.programming.JavaHelloWorld;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.stereotype.Controller;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(Controller.class)
+public class HelloControllerTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    public void hello() throws Exception {
+        mvc.perform(get("/hello"))
+                .andExpect(status().isOk());
+    }
+}
